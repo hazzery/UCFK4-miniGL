df3687225141e6066929b3fa533df585f1ef3005
Harry Parkes <harrydparkes@protonmail.com>
2022-10-25 09:10:46 +1300

    matrix_display driver

---
+++ matrix_display.h
@@ -0,0 +1,28 @@
+/**
+ *  @file   matrix_display.h
+ *  @author Harrison Parkes
+ *  @date   25th of October 2022
+ *  @brief  Driver code for matrix of single colour LEDs
+*/
+
+#ifndef MATRIX_DISPLAY_H
+#define MATRIX_DISPLAY_H
+
+#include <stdint.h>
+
+/**
+ * @brief Set all matrix PIO pins as outputs
+ * and turns them all off
+ */
+void matrix_init();
+
+
+/**
+ * @brief Show single column on the matrix display
+ *
+ * @param column the column number to be shown
+ * @param column_vals array of pixel values to shown on display column
+ */
+void show_column(uint8_t column, int8_t column_vals[]);
+
+#endif //MATRIX_DISPLAY_H
